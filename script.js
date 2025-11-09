1337f8fc0b77fbacbcae7e054d26e6720635358b
RicardoCasais <ricardocasais2@gmail.com>
2025-11-09 17:06:54 +0000

    Created the data arrays

---
+++ script.js
@@ -0,0 +1,15 @@
+const subjects = [
+  "Your creativity",
+  "Your determination",
+  "Your positive attitude",
+  "Your hard work",
+];
+
+const verbs = ["will lead to", "will inspire", "will create", "will unlock"];
+
+const outcomes = [
+  "amazing opportunities",
+  "great success",
+  "positive changes",
+  "new beginnings",
+];
