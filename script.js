f0788e793091900d135559d91f2231a0ba477b1c
RicardoCasais <ricardocasais2@gmail.com>
2025-11-10 13:22:17 +0000

    Created the base of the program

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Storing the game data.
 const subjects = [
   "Your creativity",
   "Your determination",
@@ -13,3 +14,16 @@ const outcomes = [
   "positive changes",
   "new beginnings",
 ];
+
+// Assigning the random indexes to new variables.
+const randomSubject = subjects[Math.floor(Math.random() * subjects.length)];
+const randomVerbs = verbs[Math.floor(Math.random() * verbs.length)];
+const randomOutcomes = outcomes[Math.floor(Math.random() * outcomes.length)];
+
+// DEBUG STATEMENT - console.log(randomSubject);
+// DEBUG STATEMENT - console.log(randomOutcomes);
+// DEBUG STATEMENT - console.log(randomVerbs);
+
+// Creating the "finalMessage" variable to hold the joined sentences.
+const finalMessage = `${randomSubject} ${randomVerbs} ${randomOutcomes}.`;
+console.log(finalMessage);
